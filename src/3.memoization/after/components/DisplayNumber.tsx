2ae111d87a25be003c59c8d3508a51ab0563e8b2
vannguyenv12 <npnv.vn1@gmail.com>
2025-03-09 20:53:27 +0700

    change to reference

--- src/3.memoization/after/components/DisplayNumber.tsx
+++ src/3.memoization/after/components/DisplayNumber.tsx
@@ -1,8 +1,8 @@
 import { memo } from 'react';
 
-const DisplayNumber = ({ count }: { count: number }) => {
+const DisplayNumber = ({ count }: { count: { value: number } }) => {
   console.log('DisplayNumber re-render');
-  return <h2>Count: {count}</h2>;
+  return <h2>Count: {count.value}</h2>;
 };
 
 export default memo(DisplayNumber);
--- src/3.memoization/after/screen.tsx
+++ src/3.memoization/after/screen.tsx
@@ -3,12 +3,12 @@ import { useState } from 'react';
 import DisplayNumber from './components/DisplayNumber';
 
 function ScreenMemo() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState({ value: 0 });
   const [random, setRandom] = useState(Math.random());
 
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Increment Count</button>
+      <button onClick={() => setCount({ value: 0 })}>Increment Count</button>
       <button onClick={() => setRandom(Math.random())}>Change Random</button>
       <DisplayNumber count={count} />
     </div>
