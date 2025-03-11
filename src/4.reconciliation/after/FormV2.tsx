eaed1ab8559b180fcf8e499e84123dc9a48f383a
vannguyenv12 <npnv.vn1@gmail.com>
2025-03-11 21:08:57 +0700

    actual re render

--- src/4.reconciliation/after/FormV2.tsx
+++ src/4.reconciliation/after/FormV2.tsx
@@ -25,11 +25,14 @@ function FormV2() {
           Phone
         </label>
 
-        {isPhone ? (
+        {/* {isPhone ? (
           <Input placeholder='Phone...' />
         ) : (
           <Input placeholder='Email...' />
-        )}
+        )} */}
+
+        {isPhone ? <Input placeholder='Phone...' /> : null}
+        {!isPhone ? <Input placeholder='Email...' /> : null}
 
         <input
           type='password'
@@ -40,5 +43,37 @@ function FormV2() {
     </div>
   );
 }
+// -------- BEFORE RE RENDER (INITIAL RENDER) -----------
+// const formV2 = [
+//   { type: 'label' },
+//   { type: 'label' },
+//   null, // 2
+//   { type: 'input' }, //3 email
+//   { type: 'input' }, // password
+// ];
+// -------- BEFORE RE RENDER (INITIAL RENDER) -----------
+// const formV2 = [
+//   { type: 'label' },
+//   { type: 'label' },
+//   { type: 'input' }, // 2 email
+//   null, // 3
+//   { type: 'input' }, // password
+// ];
+
+// const formV2 = {
+//   type: 'div',
+//   children: [
+//     {
+//       type: 'form',
+//       children: [
+//         { type: 'label' },
+//         { type: 'label' },
+//         null,
+//         { type: 'input' },
+//         { type: 'input' },
+//       ],
+//     },
+//   ],
+// };
 
 export default FormV2;
